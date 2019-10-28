818859689043034c9db80033ddc84218208863f1
stooperpig <stooperpig@gmail.com>
2019-10-27 22:09:10 -0500

    general data panel

--- src/components/generaldatapanel/GeneralDataPanel.js
+++ src/components/generaldatapanel/GeneralDataPanel.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './GeneralDataPanel.css';
 import ToggleButton from '../togglebutton/ToggleButton';
 import { Nationalities, Sides } from '../../constants/game-constants';
-import {updateScenarioData, updateNationalities} from './actions';
+import { updateScenarioData, updateNationalities } from './actions';
 import { connect } from 'react-redux';
+import { runInThisContext } from 'vm';
 
 class GeneralDataPanel extends React.PureComponent {
     constructor(props) {
@@ -12,32 +13,52 @@ class GeneralDataPanel extends React.PureComponent {
         this.handleSetMovesFirst = this.handleSetMovesFirst.bind(this);
         this.handleUpdateSide1Nationalities = this.handleUpdateSide1Nationalities.bind(this);
         this.handleUpdateSide2Nationalities = this.handleUpdateSide2Nationalities.bind(this);
-        this.handleUpdateScenarioName = this.handleUpdateScenarioName.bind(this);
+        this.handleUpdateScenarioData = this.handleUpdateScenarioData.bind(this);
         this.scenarioName = React.createRef();
-        this.state = {
-            scenarioName:'biteme'
-        };
+        this.scenarioId = React.createRef();
+        this.numberOfTurns = React.createRef();
+        //this.state = {
+        //    scenarioName:'biteme'
+        //};
     }
 
-    handleUpdateScenarioName(event) {
-        //this.props.updateScenarioData('name',null);
-        this.setState({...this.state, scenarioName:event.target.value});
+    componentDidMount() {
+        this.scenarioName.current.value = this.props.scenario.name;
+        this.scenarioId.current.value = this.props.scenario.id;
+        this.numberOfTurns.current.value = this.props.scenario.numberOfTurns;
+    }
+
+    handleUpdateScenarioData(event) {
+        console.log('scenarioName changed');
+        switch(event.target.name) {
+            case 'scenarioName':
+                this.props.updateScenarioData('name', this.scenarioName.current.value);
+                break;
+            case 'scenarioId':
+                this.props.updateScenarioData('id', this.scenarioId.current.value);
+                break;
+            case 'numberOfTurns':
+                this.props.updateScenarioData('numberOfTurns', parseInt(this.numberOfTurns.current.value));
+                break;
+            default:
+                break;
+        }
     }
 
     handleUpdateScenarioId() {
-        this.props.updateScenarioData('id',null);
+        this.props.updateScenarioData('id', this.scenarioId.current.value);
     }
 
     handleUpdateNumberOfTurns() {
-        this.props.updateScenarioData('numberOfTurns',0);
+        this.props.updateScenarioData('numberOfTurns', parseInt(this.numberOfTurns.current.value));
     }
 
     handleSetSetsupFirst(side) {
-        this.props.updateScenarioData('setsupFirst',side);
+        this.props.updateScenarioData('setsupFirst', side);
     }
 
     handleSetMovesFirst(side) {
-        this.props.updateScenarioData('movesFirst',side);
+        this.props.updateScenarioData('movesFirst', side);
     }
 
     handleUpdateSide1Nationalities(nationality) {
@@ -50,20 +71,22 @@ class GeneralDataPanel extends React.PureComponent {
 
     buildNationalityFlags(nationalityCodes) {
         return {
-            AMERICAN: (nationalityCodes.includes(Nationalities.AMERICAN.code))?Nationalities.AMERICAN.code:null,
-            GERMAN: (nationalityCodes.includes(Nationalities.GERMAN.code))?Nationalities.GERMAN.code:null,
-            RUSSIAN: (nationalityCodes.includes(Nationalities.RUSSIAN.code))?Nationalities.RUSSIAN.code:null
+            AMERICAN: (nationalityCodes.includes(Nationalities.AMERICAN.code)) ? Nationalities.AMERICAN.code : null,
+            GERMAN: (nationalityCodes.includes(Nationalities.GERMAN.code)) ? Nationalities.GERMAN.code : null,
+            RUSSIAN: (nationalityCodes.includes(Nationalities.RUSSIAN.code)) ? Nationalities.RUSSIAN.code : null
         };
     }
 
     render() {
+        console.log('render generaldata panel')
         let side1Nationalities = this.buildNationalityFlags(this.props.scenario.side1.nationalityCodes);
         let side2Nationalities = this.buildNationalityFlags(this.props.scenario.side2.nationalityCodes);
 
         return (
             <div className="general-data-panel">
-                <label>Scenario Name: <input type="text" value={this.state.scenarioName} onChange={this.handleUpdateScenarioName} /></label>  <label>Scenario Id: <input type="text" /></label><br />
-                <label>Number of Turns: <input type="text" /></label><br />
+                <label>Scenario Name: <input type="text" name="scenarioName" ref={this.scenarioName} onBlur={this.handleUpdateScenarioData} /></label>
+                <label>Scenario Id: <input type="text" name="scenarioId" ref={this.scenarioId} onBlur={this.handleUpdateScenarioData} /></label><br />
+                <label>Number of Turns: <input type="text" name="numberOfTurns" ref={this.numberOfTurns} onBlur={this.handleUpdateScenarioData} /></label><br />
                 <label>
                     Side 1 Nationalities:
                     <ToggleButton label={Nationalities.AMERICAN.label} activationValue={Nationalities.AMERICAN.code} currentValue={side1Nationalities.AMERICAN} handleClick={this.handleUpdateSide1Nationalities} />
